4c4f2cf4d3cb0deaf8eab00ef0f452eeb8bc1e1d
ValeryOrtiz <valery.comortiz@gmail.com>
2024-08-26 10:27:18 -0500

    getters y setters habitacion

--- src/main/java/co/edu/uniquindio/programacion2/Hotel/Model/Habitacion.java
+++ src/main/java/co/edu/uniquindio/programacion2/Hotel/Model/Habitacion.java
@@ -6,12 +6,32 @@ public class Habitacion {
     private Integer numero;
     private TipoHabitacion tipoHabitacion;
     private float precio;
+    
     public Habitacion(Integer numero, TipoHabitacion tipoHabitacion, float precio) {
         this.numero=numero;
         this.tipoHabitacion=tipoHabitacion;
         this.precio=precio;
 
     }
+    public Integer getNumero() {
+        return numero;
+    }
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+    public TipoHabitacion getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+    public void setTipoHabitacion(TipoHabitacion tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+    public float getPrecio() {
+        return precio;
+    }
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    
 
 
 
